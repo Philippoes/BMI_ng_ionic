03c18e47df641f16248b54869485b012855b1c07
Philip <Philip.zdb@gmail.com>
2017-01-17 13:25:18 +0100

    Fixes issue with app.js

--- www/js/app.js
+++ www/js/app.js
@@ -45,10 +45,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
                 controller: 'AboutController'
               }
             }
-          });
+          })
+      });
     // Each tab has its own nav history stack:
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/tab/dash');
     $urlRouterProvider.otherwise('/tab/about');
-  })
-    ;
+  });
