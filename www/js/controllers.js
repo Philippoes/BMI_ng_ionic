4192f3430195fcaf746f431e3d4ed8b032adffa4
Amber <amber@amberwilkie.com>
2017-01-17 20:46:26 +0100

    Set imperialSwitch from settings page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,17 +1,26 @@
 angular.module('starter.controllers', [])
 
   .controller('BmiController', function ($scope) {
+
     $scope.data = {};
+    $scope.data.imperialSwitch = false;
+    console.dir($scope.data);
+
     $scope.calculateBMI = function () {
       var person = new Person({
         weight: $scope.data.weight,
         height: $scope.data.height,
         height_feet: $scope.data.height_feet,
         height_inches: $scope.data.height_inches,
-        imperialswitch: $scope.data.imperialswitch
+        imperialSwitch: $scope.data.imperialSwitch
       });
       person.calculate_bmi();
       $scope.person = person;
+    }
+
+    $scope.setUnits = function () {
+      console.log(this.data.imperialSwitch);
+      $scope.data.imperialSwitch = this.value;
     }
   })
 
